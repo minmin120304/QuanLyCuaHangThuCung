510eb0384aad0258932db9676fe0323a97f1cf0b
Nguyen Anh Ngoc Minh <22010476@st.phenikaa-uni.edu.vn>
2025-03-04 18:15:11 +0700

    Update

---
+++ QuanLyCuaHangThuCung/Migrations/202503041053353_All.cs
@@ -0,0 +1,28 @@
+﻿namespace QuanLyCuaHangThuCung.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class All : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employee",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        employeeName = c.String(maxLength: 50),
+                        address = c.String(maxLength: 50),
+                        phoneNum = c.String(maxLength: 12),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employee");
+        }
+    }
+}
