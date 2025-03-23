f4def8dd0e7d8bc115aa5173bb5067b0c402138d
LeThuy <122143783+LimerLil@users.noreply.github.com>
2025-03-23 03:24:36 +0000

    Create LogoutBTN + Create HomePage + fix mini bugs

--- QuanLyCuaHangThuCung/MainWindow.xaml.cs
+++ QuanLyCuaHangThuCung/MainWindow.xaml.cs
@@ -63,5 +63,12 @@ namespace QuanLyCuaHangThuCung
         {
             MainContent.Content = new Bill();
         }
+        
+        private void DX_Click(object sender, RoutedEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Close();
+        }
     }
 }
