daf7666120c0830195af9112f14266691fe3121b
Nguyen Anh Ngoc Minh <22010476@st.phenikaa-uni.edu.vn>
2025-03-31 01:10:45 +0700

    Add Migrations

---
+++ QuanLyCuaHangThuCung/Migrations/202503301805196_Test1.cs
@@ -0,0 +1,16 @@
+﻿namespace QuanLyCuaHangThuCung.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Test1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
